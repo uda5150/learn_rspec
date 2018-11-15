f3baab74e317bd27e7420f8ff9da9df742f2817c
uda5150 <tks.udagawa@gmail.com>
2018-11-15 09:26:30 +0900

    letの導入

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,21 +2,26 @@ require 'spec_helper'
 
 RSpec.describe User do
   describe '#greet' do
-
-    before do
-      @params = { name: 'たろう'}
-    end
+    let(:params){{ name: 'たろう' }}
 
     context '12歳以下の場合' do
+      before do
+        params.merge!(age:12)
+      end
+
       it 'ひらがなで答えること' do
-        user = User.new(@params.merge(age: 12))
+        user = User.new(params)
         expect(user.greet).to eq 'ぼくはたろうだよ。'
       end
     end
 
     context '13歳以上の場合' do
+      before do
+        params.merge!(age:13)
+      end
+
       it '漢字で答えること' do
-        user = User.new(@params.merge(age: 13))
+        user = User.new(params)
         expect(user.greet).to eq '僕はたろうです。'
       end
     end
