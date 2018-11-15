429a1f82e17919e17018878b9833be284ad4bd8b
uda5150 <tks.udagawa@gmail.com>
2018-11-15 09:42:35 +0900

    subjectの導入

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,17 +5,19 @@ RSpec.describe User do
     let(:params){{ name: 'たろう', age: age }}
     let(:user){ User.new(params) }
 
+    subject { user.greet }
+
     context '12歳以下の場合' do
       let(:age) { 12 }
       it 'ひらがなで答えること' do
-        expect(user.greet).to eq 'ぼくはたろうだよ。'
+        is_expected.to eq 'ぼくはたろうだよ。'
       end
     end
 
     context '13歳以上の場合' do
       let(:age) { 13 }
       it '漢字で答えること' do
-        expect(user.greet).to eq '僕はたろうです。'
+        is_expected.to eq '僕はたろうです。'
       end
     end
   end
