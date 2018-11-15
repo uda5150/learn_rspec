a78cdd103b990c9c09edd39ad4c70a746acec3bd
uda5150 <tks.udagawa@gmail.com>
2018-11-15 23:35:33 +0900

    shared_examplesとit_behaves_likeの追加

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,14 +6,33 @@ RSpec.describe User do
 
     subject { user.greet }
 
-    context '12歳以下の場合' do
-      let(:age) { 12 }
+    shared_examples '子どものあいさつ' do
       it { is_expected.to eq 'ぼくはたろうだよ。'}
     end
 
-    context '13歳以上の場合' do
-      let(:age) { 13 }
+    context '0歳の場合' do
+      let(:age) { 0 }
+      it_behaves_like '子どものあいさつ'
+    end
+
+    context '12歳の場合' do
+      let(:age) { 12 }
+      it_behaves_like '子どものあいさつ'
+    end
+
+
+    shared_examples '大人のあいさつ' do
       it { is_expected.to eq '僕はたろうです。'}
+    end
+
+    context '13歳の場合' do
+      let(:age) { 13 }
+      it_behaves_like '大人のあいさつ'
+    end
+
+    context '100歳の場合' do
+      let(:age) { 100 }
+      it_behaves_like '大人のあいさつ'
     end
   end
 end
